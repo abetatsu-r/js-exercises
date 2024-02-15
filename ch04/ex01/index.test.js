166c97859d6c5d56a8c3b27a567435f3e81290f9
abetatsu-r <tatsuya.abe@jp.ricoh.com>
2024-02-15 15:53:55 +0900

    testcase記載漏れ

--- ch04/ex01/index.test.js
+++ ch04/ex01/index.test.js
@@ -1,4 +1,4 @@
-import { add } from "./index.js"; // typescript で書く場合は "./index.ts"
+import { add, sub, div, mul } from "./index.js"; // typescript で書く場合は "./index.ts"
 
 function addTestCase(c1, c2, expected) {
   return [c1, c2, expected];
@@ -68,7 +68,37 @@ test.each([
   addTestCase(a1, c1, { re: 2, im: 2 }),
   addTestCase(c1, a1, { re: 2, im: 2 }),
   addTestCase(a1, a1, { re: 2, im: 2 }),
-  addTestCase(a1, a2, 2),
 ])("add(%p, %p) => %p", (n1, n2, expected) => {
   expect(equal(add(n1, n2), expected)).toBe(true);
 });
+
+test.each([
+  subTestCase(1, 1, 0),
+  subTestCase(1, -1, 2),
+  subTestCase(c1, c1, 0),
+  subTestCase(c1, 1, {im : 1}),
+  subTestCase(1, c1, { im: -1}),
+  subTestCase(c1, c2, {im: 2})
+])("sub(%p, %p) => %p", (n1, n2, expected) => {
+  expect(equal(sub(n1, n2), expected)).toBe(true);
+})
+
+test.each([
+  mulTestCase(c1, c1, {im: 2}) ,
+  mulTestCase(c1, -1, {re: -1, im: -1}),
+  mulTestCase(2, c1, {re: 2, im: 2}),
+  mulTestCase(1, 2, 2),
+  mulTestCase(c1, 0, 0)
+])("mul(%p, %p) => %p", (n1, n2, expected) => {
+  expect(equal(mul(n1, n2), expected)).toBe(true);
+})
+
+test.each([
+  divTestCase(c1, c1, 1) ,
+  divTestCase(c1, -1, {re: -1, im: -1}),
+  divTestCase(2, c1, {re: 2, im: 2}),
+  divTestCase(1, 2, 0.5),
+  divTestCase(0, c1, 0)
+])("div(%p, %p) => %p", (n1, n2, expected) => {
+  expect(equal(div(n1, n2), expected)).toBe(true);
+})
