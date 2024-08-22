7e55929fe48bd2a6f2ea6ccda60fc1f7abd88ca3
abetatsu-r <tatsuya.abe@jp.ricoh.com>
2024-08-22 13:46:31 +0900

    rest

--- ch15.04-10/ex09/index.js
+++ ch15.04-10/ex09/index.js
@@ -33,39 +33,52 @@ document.getElementById("image").addEventListener("change", (event) => {
     // imageData の中身はそのままに別の配列に結果を格納するとよい
     // ```js
     const outputData = new Uint8ClampedArray(imageData.data.length);
+    //
+    const gausian = [
+      [1, 4, 6, 4, 1],
+      [4, 16, 24, 16, 4],
+      [6, 24, 36, 24, 6],
+      [4, 16, 24, 16, 4],
+      [1, 4, 6, 4, 1],
+    ];
 
-    // 仮置き(netから引っ張ってきた値、σ=0.85くらいらしい)
-    const gausianFilter = [
-      1, 4, 6, 4, 1,
-      4, 16, 24, 16, 4,
-      6, 24, 36, 24, 6,
-      4, 16, 24, 16, 4,
-      1, 4, 6, 4, 1,
-    ].map(x => x /256);
-
+    console.log("width: " + img.width);
     // TODO: ここで imageData.data を参照して outputData に結果を格納
-    // だるいんで後回し
+    // WARNING: 超遅い
     for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      const focused_x = (i / 4) % width;
-      const focused_y = (i / 4 - x) / width;
-      
-      let sum = [0, 0, 0, 0];
-      for (let x = -2; x <= 2; x++) {
-        for (let y = -2; y <= 2; y++) {
-          // 0padding
-          if (focused_x + x < 0 || focused_x + x >= width) {
+      const row = Math.floor(i / 4) % img.width;
+      const col = Math.floor(i / (4 * img.width));
+
+      let r = 0;
+      let g = 0;
+      let b = 0;
+      for (let ir = 0; ir < 5; ir++) {
+        for (let ic = 0; ic < 5; ic++) {
+          const current_row = row + ir - 2;
+          const current_col = col + ic - 2;
+          if (
+            current_row < 0 ||
+            current_row >= img.width ||
+            current_col < 0 ||
+            current_col >= img.height
+          ) {
             continue;
           }
 
-          if (focused_y + y < 0 || focused_y + y >= height) {
-            continue;
-          }
+          let index = (current_col * img.width + current_row) * 4;
 
-          sum =
+          r += (data[index] * gausian[ir][ic]) / 256;
+          g += (data[index + 1] * gausian[ir][ic]) / 256;
+          b += (data[index + 2] * gausian[ir][ic]) / 256;
         }
       }
+
+      outputData[i] = r;
+      outputData[i + 1] = g;
+      outputData[i + 2] = b;
+      outputData[i + 3] = data[i + 3];
     }
+
     const outputImageData = new ImageData(outputData, img.width, img.height);
     filteredCtx.putImageData(outputImageData, 0, 0);
   });
